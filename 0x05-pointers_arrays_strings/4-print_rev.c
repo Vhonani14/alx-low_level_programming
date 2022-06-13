5297a3c52ba27da5fd18758035fd78ae3fa9f8ce
Vhonani14 <vhonanimufeba@gmail.com>
2022-06-13 14:11:25 -0700

    Task

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,12 +9,11 @@ void print_rev(char *s)
 		int len;
 
 			for (len = 0; s[len] != '\0'; len++)
-			{			
-			}
-		for (len = len - 1; len >= 0; len--
-                {
-		_putchar(s[len]);
-		}
-                _putchar('\n');
-
+					{
+							}
+				for (len = len - 1; len >= 0; len--)
+						{
+									_putchar(s[len]);
+										}
+					_putchar('\n');
 }
