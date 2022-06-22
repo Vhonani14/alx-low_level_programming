32c606c3bebae26c19e297bcdbcf92e644d5946c
Vhonani14 <vhonanimufeba@gmail.com>
2022-06-22 15:27:05 -0700

    0-puts_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,8 +1,8 @@
-#ifndef MAIN_H
+#ifndef MAIN_h
 #define MAIN_H
 
-int _putchar (char c);
-int _puts_recursion (char *s);
+int _putchar(char c);
+int _puts_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
