32c4b2ddb7f8f4851e4010fee6afa204b5062b4a
Vhonani14 <vhonanimufeba@gmail.com>
2022-06-28 15:25:12 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -5,16 +5,16 @@
  * @ac: arguement count
  * @av: pointer to arguements
  * Return: pointer to new space in memory or null
- */
+ **/
 char *argstostr(int ac, char **av)
 {
 	char *strDup;
 	int i, j, k, size;
+	
 	if (ac == 0 || av == NULL)
 		return (NULL);
 	size = 0;
-/** count the number of chars in each string
- */
+/* count the number of chars in each string */
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
@@ -22,7 +22,7 @@ char *argstostr(int ac, char **av)
 		size++;
 	}
 	size++;
-/** allocate memory for total number of chars and
+/* allocate memory for total number of chars and
  * new line for each word
  */
 	strDup = malloc(sizeof(char) * size);
